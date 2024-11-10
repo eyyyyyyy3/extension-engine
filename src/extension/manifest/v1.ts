dfe43946c0306bcf889815fa41f2b45fea6d46a9
eyyyyyyy3@gmail.com <eyyyyyyy3@gmail.com>
2024-11-10 15:44:05 +0100

    Changed the way of how extensionDepedenies are represented
    Back then it was just the identifier of another extension but now it is
    the identifier and also the version of the extension

--- src/extension/manifest/v1.ts
+++ src/extension/manifest/v1.ts
@@ -1,34 +1,35 @@
-import { string, z } from "zod";
+import { z } from "zod";
 
 const runtimeEnum = z.enum(["native", "web", "universal"]);
 
 //Manifest description for Manifest version 1
 export const schema = z.object({
-  manifestVersion: z.literal("1.0"),                          //Manifest Version. Predefined values which we make public to developers
-  apiVersion: z.string(),                                     //The API version required to run this extension. Version must be parseable by node-semve
-  runtime: runtimeEnum,                                       //Where the extension is intended to run. Native has fs access for example
-  name: z.string(),                                           //The extensions name
-  publisher: z.string(),                                      //The extension publisher. This is also unique t the Marketplace
-  version: z.string(),                                        //The version of the current extension. Version must be parseable by node-semver
-  license: z.string(),                                        //https://docs.npmjs.com/cli/v7/configuring-npm/package-json#license
-  displayName: z.string(),                                    //The display name for the extension used in the Marketplace. It is unique
-  description: z.string().optional(),                         //A description of the extension
-  sponsor: z.string().url().optional(),                       //A link for people if they want to sponsor the extension
+  manifestVersion: z.literal("1.0"),                                              //Manifest Version. Predefined values which we make public to developers
+  apiVersion: z.string(),                                                         //The API version required to run this extension. Version must be parseable by node-semve
+  runtime: runtimeEnum,                                                           //Where the extension is intended to run. Native has fs access for example
+  name: z.string(),                                                               //The extensions name
+  publisher: z.string(),                                                          //The extension publisher. This is also unique t the Marketplace
+  version: z.string(),                                                            //The version of the current extension. Version must be parseable by node-semver
+  license: z.string(),                                                            //https://docs.npmjs.com/cli/v7/configuring-npm/package-json#license
+  displayName: z.string(),                                                        //The display name for the extension used in the Marketplace. It is unique
+  description: z.string().optional(),                                             //A description of the extension
+  sponsor: z.string().url().optional(),                                           //A link for people if they want to sponsor the extension
   entrypoint: z.string().refine(
     (val) => val.endsWith(".js"),
     { message: "[MANIFEST] The entrypoint file has to end with .js!" }
-  ),                                                          //The path/name of the entrypoint of the extension
-  extensionDependencies: z.string().array().optional(),       //If extensions depend on other extensions, they can be defined in here
+  ),                                                                              //The path/name of the entrypoint of the extension
+  //TODO: Check if the version is according to the node-semve specification
+  extensionDependencies: z.tuple([z.string(), z.string()]).array().optional(),    //If extensions depend on other extensions, they can be defined in here. It goes [identifier, version]
   icon: z.string().refine(
     (val) => val.endsWith(".png"),
     { message: "[MANIFEST] The icon file has to end with .png!" }
-  ),                                                          //The path to the icon of the extension. Check size later. The format is png
-  autoLoad: z.boolean().optional().default(false),            //Should the extension be loaded automatically? Default: false
+  ),                                                                              //The path to the icon of the extension. Check size later. The format is png
+  autoLoad: z.boolean().optional().default(false),                                //Should the extension be loaded automatically? Default: false
   ui: z.record(z.string().refine(
     (val) => val.endsWith(".html"),
     { message: "[MANIFEST] UI files have to end with .html!" }
-  )).optional(),                                              //Similar to figmas ui definition
-  developmentKey: z.string().optional(),                      //Used for partners who want access to the DB while in development
+  )).optional(),                                                                  //Similar to figmas ui definition
+  developmentKey: z.string().optional(),                                          //Used for partners who want access to the DB while in development
 })
 
 
@@ -45,7 +46,7 @@ export interface IManifest {
   description(): string | undefined;
   sponsor(): string | undefined;
   entrypoint(): string;
-  extensionDependencies(): string[] | undefined;
+  extensionDependencies(): [string, string][] | undefined;
   icon(): string;
   autoLoad(): boolean;
   ui(): Record<string, string> | undefined;
@@ -95,7 +96,7 @@ export class Manifest implements IManifest {
     //TODO: Sanitize the so that no path traversal is possible
     return this.#parsedData.entrypoint;
   }
-  extensionDependencies(): string[] | undefined {
+  extensionDependencies(): [string, string][] | undefined {
     return this.#parsedData.extensionDependencies;
   }
   icon(): string {
